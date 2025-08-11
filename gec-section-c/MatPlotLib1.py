2a2568c8081edcc4c675b13b97df6133e4d14d16
Swarup <support@mysideincome.com>
2025-08-11 10:13:07 +0530

    mat plot lib

---
+++ gec-section-c/MatPlotLib1.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import matplotlib.pyplot as plt
+import pandas
+
+spark = SparkSession.builder.appName("matPlotLib1").getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+
+schema = StructType([
+    StructField("Product",StringType(),False),
+    StructField("Quantity",IntegerType(),True),
+    StructField("Sales",IntegerType(),False)
+])
+
+df = spark.read.csv("../dataset/sales.csv", header=True, sep=",",schema=schema)
+df.show()
+
+## First mat plot.[Line chat]
+dfMatPlot = df.groupBy("Product").sum("Sales")
+dfMatPlot.show()
+x = dfMatPlot.toPandas()["Product"].values.tolist()
+y = dfMatPlot.toPandas()["sum(Sales)"].values.tolist()
+plt.plot(x, y)
+plt.show()
